671b4b76b82a3177326273807a99759644d665c8
shailju <tripathishailja326@gmail.com>
2026-07-01 18:26:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar">
+        <h2>🔮 Vedic Astro Kashi</h2>
+
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
+      <section className="hero">
+        <h1>Discover Your Destiny</h1>
+
+        <h2>Through Ancient Vedic Astrology</h2>
+
+        <p>
+          Personalized guidance for career, marriage, health,
+          education and life decisions.
+        </p>
+
+        <button>Book Consultation</button>
+      </section>
+    </>
+  );
+}
+
+export default App;
